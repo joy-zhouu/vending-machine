13fe2152596a4dc3c4e92e83a5d650b2d0c11631
joy-zhouu <joy.jy.zhou@gmail.com>
2023-05-31 13:43:56 -0400

    UI classes updated

--- src/main/java/com/sg/vendingmachine/ui/VendingMachineView.java
+++ src/main/java/com/sg/vendingmachine/ui/VendingMachineView.java
@@ -1,7 +1,10 @@
 package com.sg.vendingmachine.ui;
 
+import com.sg.vendingmachine.dto.Change;
+import com.sg.vendingmachine.dto.CoinValue;
 import com.sg.vendingmachine.dto.Product;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class VendingMachineView {
@@ -9,43 +12,61 @@ public class VendingMachineView {
   public VendingMachineView(UserIO io) {
     this.io = io;
   }
-
-  public int printMenu(List<Product> productList) {
-    io.print("Welcome to the Vending Machine!");
-    io.print("No     Product          Price");
-    io.print("-----------------------------\n");
-
-    // Iterate through list of items and display them and their prices
-    for (Product currentProduct: productList) {
-      int index = productList.indexOf(currentProduct);
-      String productInfo = String.format(index + "     %s          $%s",
-              currentProduct.getProductName(),
-              currentProduct.getPrice());
-      io.print(productInfo);
-    }
-
-    return io.readInt("Please enter how much money you have (in dollars)");
+  public void displayVendingMachineWelcome() {
+    io.print("Welcome to the Vending Machine!\n");
   }
 
-  public int getSelection() {
-    return io.readInt("Please enter which item you'd like to purchase");
+  public void displayProductHeader() {
+    io.print("No\tProduct\t\tPrice");
+    io.print("------------------------------");
   }
 
-  public void displayPurchaseSuccess() {
-    io.print("Purchase successful.");
-    // Print change
+  public void displayProduct(Product product) {
+    io.print(product.getProductId() + "\t" + product.getProductName());
   }
 
-  public String getContinuePurchase() {
-    return io.readString("Would you like to make another purchase?");
+  public BigDecimal promptUserMoneyInput() {
+    return io.readBigDecimal("\nPlease put in how much money you have: ");
   }
 
-  public void displayExitBanner() {
-    io.print("Good Bye!!");
+  public String promptUserProductChoice() {
+    return io.readString("Please select which product you'd like to purchase: ");
+  }
+
+  public void displayUserChoiceOfProduct(Product product) {
+    io.print("You have chosen " + product.getProductName() + ".");
+  }
+
+  public void displayUserMoneyInput(BigDecimal amount) {
+    io.print("You have deposited $" + amount + ".");
+  }
+
+  public void displayChangeDue(Change change) {
+    io.print("Change due: ");
+    io.print(CoinValue.QUARTERS + ": " + change.getQuarters());
+    io.print(CoinValue.DIMES + ": " + change.getDimes());
+    io.print(CoinValue.NICKELS + ": " + change.getNickels());
+    io.print(CoinValue.PENNIES + ": " + change.getPennies());
   }
 
   public void displayErrorMessage(String errorMsg) {
     io.print("=== ERROR ===");
     io.print(errorMsg);
+  }
+
+  public boolean toExit() {
+    String answer = io.readString("Do you want to exit the vending machine?");
+    if (answer.startsWith("y")) {
+      return true;
+    }
+    return false;
+  }
+
+  public void displayFinalMessage() {
+    io.print("Goodbye!");
+  }
+
+  public void displayUserResponse() {
+    io.print("Do you want to make another selection?");
   }
 }
