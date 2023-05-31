4f0b7cfd17cc6deb236cd9cf7ad8a551b38ec2e2
Negar Lashgari <negar.lashgari@gmail.com>
2023-05-31 16:21:12 -0400

    change

--- src/main/java/com/sg/vendingmachine/dto/Change.java
+++ src/main/java/com/sg/vendingmachine/dto/Change.java
@@ -9,13 +9,13 @@ public class Change {
     private int pennies;
 
     public Change(BigDecimal amount){
-        this.quarters = amount.divide(new BigDecimal("25")).intValue();
-        amount = amount.remainder(new BigDecimal("25"));
-        this.dimes = amount.divide(new BigDecimal("10")).intValue();
-        amount = amount.remainder(new BigDecimal("10"));
-        this.nickels = amount.divide(new BigDecimal("5")).intValue();
-        amount = amount.remainder(new BigDecimal("5"));
-        this.pennies = amount.divide(new BigDecimal("1")).intValue();
+        this.quarters = amount.divide(new BigDecimal("0.25")).intValue();
+        amount = amount.remainder(new BigDecimal("0.25"));
+        this.dimes = amount.divide(new BigDecimal("0.10")).intValue();
+        amount = amount.remainder(new BigDecimal("0.10"));
+        this.nickels = amount.divide(new BigDecimal("0.05")).intValue();
+        amount = amount.remainder(new BigDecimal("0.05"));
+        this.pennies = amount.divide(new BigDecimal("0.01")).intValue();
     }
 
     //we don't need setters because we use constructor to initialize the properties.
