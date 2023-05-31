3bfc3a50cd19983093673a94cc06f61770ca6c2c
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-05-31 11:29:01 -0400

    setup test directory and added DAO test

---
+++ src/test/java/VendingMachineDaoImplTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VendingMachineDaoImplTest {
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void getAllProductIds() {
+    }
+
+    @Test
+    void getProduct() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+
+    @Test
+    void removeProduct() {
+    }
+
+    @Test
+    void loadProductsFromFile() {
+    }
+
+    @Test
+    void writeProductsToFile() {
+    }
+}
