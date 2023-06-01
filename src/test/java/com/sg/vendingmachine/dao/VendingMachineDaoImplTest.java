affc300afdcd8cebc6c7c0c5cbe696bb9a3f476c
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-06-01 11:24:38 -0400

    finished 4 methods in DaoImplTest file

--- src/test/java/com/sg/vendingmachine/dao/VendingMachineDaoImplTest.java
+++ src/test/java/com/sg/vendingmachine/dao/VendingMachineDaoImplTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileWriter;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -35,8 +36,7 @@ class VendingMachineDaoImplTest {
 //      Add the products to the DAO
         testDao.addProduct(chips.getProductId(), chips);
 
-//        testDao.addProduct("1", chips);
-//        Get the Student from the DAO
+//        Get the Product from the DAO
         Product retrievedProduct= testDao.getProduct(chips.getProductId());
 
         // Check the data is equal
@@ -47,15 +47,50 @@ class VendingMachineDaoImplTest {
     }
 
     @Test
-    void getAllProducts() {
+    void addGetAllProducts() throws VendingMachinePersistenceException {
+        // create 2 products
+        Product chips = new Product("1","Chips",new BigDecimal("2.00"),10);
+        Product water = new Product("2","Water",new BigDecimal("1.00"),10);
+
+        // Add both our products to the DAO
+        testDao.addProduct(chips.getProductId(), chips);
+        testDao.addProduct(water.getProductId(), water);
+
+        // Retrieve the list of all products within the DAO
+        List<Product> allProducts = testDao.getAllProducts();
+
+        // First check the general contents of the list that it isn't null, then that there are 2 products
+        assertNotNull(allProducts,"The list of products must not be null" );
+        assertEquals(2, allProducts.size(), "List of products should have 2 products");
+
+        // Then the specifics of each product are contained within the List
+        assertTrue(testDao.getAllProducts().contains(chips),"The list of products should include chips");
+        assertTrue(testDao.getAllProducts().contains(water), "The list of products should include water");
     }
 
     @Test
-    void getAllProductIds() {
-    }
+    void addGetAllProductIds() throws VendingMachinePersistenceException {
+        // create 3 products
+        Product chips = new Product("1","Chips",new BigDecimal("2.00"),10);
+        Product water = new Product("2","Water",new BigDecimal("1.00"),10);
+        Product candy = new Product("3","Candy",new BigDecimal("2.00"),10);
 
-    @Test
-    void getProduct() {
+        // Add products to the DAO
+        testDao.addProduct(chips.getProductId(), chips);
+        testDao.addProduct(water.getProductId(), water);
+        testDao.addProduct(candy.getProductId(), candy);
+
+        // Retrieve the list of all products IDS within the DAO
+        List<String> allProducts = testDao.getAllProductIds();
+
+        // First check the general contents of the list that it isn't null, then that there are 3 products
+        assertNotNull(allProducts,"The list of products must not be null" );
+        assertEquals(3, allProducts.size(), "List of products should have 3 products");
+
+        // Then the specifics of each product are contained within the List
+        assertTrue(testDao.getAllProductIds().contains(chips.getProductId()),"The list of products should include chips as #1");
+        assertTrue(testDao.getAllProductIds().contains(water.getProductId()),"The list of products should include water as #2");
+        assertTrue(testDao.getAllProductIds().contains(candy.getProductId()),"The list of products should include candy as #3");
     }
 
     @Test
